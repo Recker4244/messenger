783bf3a67f3479e20644f20a15614c4434e1fb34
Ashutosh <ashutoshsenapati2311@gmail.com>
2025-12-26 02:13:09 +0530

    chore: refactor

--- lib/ui/views/offers/offers_view.dart
+++ lib/ui/views/offers/offers_view.dart
@@ -1,40 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
-import '../../viewmodels/offers_viewmodel.dart';
 
 class OffersView extends StatelessWidget {
   const OffersView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ViewModelBuilder<OffersViewModel>.reactive(
-      viewModelBuilder: () => OffersViewModel(),
-      onModelReady: (model) => model.initialize(),
-      builder: (context, model, child) {
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text('Offers'),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(
-                  Icons.local_offer,
-                  size: 64,
-                  color: Colors.orange,
-                ),
-                const SizedBox(height: 16),
-                Text(
-                  model.title,
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ],
-            ),
-          ),
-        );
-      },
+    return Scaffold(
+      appBar: AppBar(title: const Text('Offers')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(Icons.local_offer, size: 64, color: Colors.orange),
+            const SizedBox(height: 16),
+            Text('Offers', style: Theme.of(context).textTheme.headlineMedium),
+          ],
+        ),
+      ),
     );
   }
 }
-
--- lib/ui/views/settings/settings_view.dart
+++ lib/ui/views/settings/settings_view.dart
@@ -1,40 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
-import '../../viewmodels/settings_viewmodel.dart';
 
 class SettingsView extends StatelessWidget {
   const SettingsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ViewModelBuilder<SettingsViewModel>.reactive(
-      viewModelBuilder: () => SettingsViewModel(),
-      onModelReady: (model) => model.initialize(),
-      builder: (context, model, child) {
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text('Settings'),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(
-                  Icons.settings,
-                  size: 64,
-                  color: Colors.grey,
-                ),
-                const SizedBox(height: 16),
-                Text(
-                  model.title,
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ],
-            ),
-          ),
-        );
-      },
+    return Scaffold(
+      appBar: AppBar(title: const Text('Settings')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(Icons.settings, size: 64, color: Colors.grey),
+            const SizedBox(height: 16),
+            Text('Settings', style: Theme.of(context).textTheme.headlineMedium),
+          ],
+        ),
+      ),
     );
   }
 }
-
